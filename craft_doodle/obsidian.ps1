c9544b9206d0ce9a2cce65d63aec759c4af0913e
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2021-03-03 20:03:45 +0700

    Update obsidian.ps1

--- craft_doodle/obsidian.ps1
+++ craft_doodle/obsidian.ps1
@@ -1,11 +1,11 @@
 # Create a Shortcut with Windows PowerShell
-$SourceFileLocation = "C:\Users\DELL\Desktop\craft_doodle\openobsidian.bat"
-$ShortcutLocation = "C:\Users\DELL\Desktop\craft_doodle\obsidian.lnk"
+$SourceFileLocation = "C:\Users\%username%\Desktop\craft_doodle\openobsidian.bat"
+$ShortcutLocation = "C:\Users\%username%\Desktop\craft_doodle\obsidian.lnk"
 #New-Object : Creates an instance of a Microsoft .NET Framework or COM object.
 #-ComObject WScript.Shell: This creates an instance of the COM object that represents the WScript.Shell for invoke CreateShortCut
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
 $Shortcut.TargetPath = $SourceFileLocation
-$Shortcut.IconLocation = "C:\Users\DELL\Desktop\craft_doodle\obsidian.ico"
+$Shortcut.IconLocation = "C:\Users\%username%\Desktop\craft_doodle\obsidian.ico"
 #Save the Shortcut to the TargetPath
 $Shortcut.Save()
